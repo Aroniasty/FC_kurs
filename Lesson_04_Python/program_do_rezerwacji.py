461cc9f26b6d126bb9d7bbef039475c32032b665
Aroniasty <aroniasty@gmail.com>
2023-10-14 14:18:10 +0200

    Lesson_04

--- Lesson_04_Python/program_do_rezerwacji.py
+++ Lesson_04_Python/program_do_rezerwacji.py
@@ -14,14 +14,14 @@ rzad_2_miejsce_5 = False
 rzad_2_miejsce_6 = False
 rzad_2_miejsce_7 = False
 rzad_2_miejsce_8 = False
-rzad_3_miejsce_1 = False
-rzad_3_miejsce_2 = False
-rzad_3_miejsce_3 = False
-rzad_3_miejsce_4 = False
-rzad_3_miejsce_5 = False
-rzad_3_miejsce_6 = False
-rzad_3_miejsce_7 = False
-rzad_3_miejsce_8 = False
+# rzad_3_miejsce_1 = False
+# rzad_3_miejsce_2 = False
+# rzad_3_miejsce_3 = False
+# rzad_3_miejsce_4 = False
+# rzad_3_miejsce_5 = False
+# rzad_3_miejsce_6 = False
+# rzad_3_miejsce_7 = False
+# rzad_3_miejsce_8 = False
 
 '''
 Napisz program do zarządzania rezerwacją miejsc w kinie.
@@ -58,41 +58,135 @@ co powinniśmy trzymać w naszym globalnym stanie
 
 print("Witaj w naszym systemie rezerwacji/sprzedży biletów")
 
-ilosc_miejsc = 24
-ilosc_rzedow = 3
+ilosc_miejsc = 16
+ilosc_rzedow = 2
 ilosc_miejsc_sprzedanych_1_rzedzie = 0
 ilosc_miejsc_sprzedanych_2_rzedzie = 0
-ilosc_miejsc_sprzedanych_3_rzedzie = 0
+#ilosc_miejsc_sprzedanych_3_rzedzie = 0
 
 koniec_programu = False
 
 while not koniec_programu:
-
-    rzad_uzytkownika = input("Podaj rząd, w którym chcesz zarezerwować miejsce ")
-    if not int(rzad_uzytkownika) in range (1,4):
-        print("Niestety nie mamy takiego rzędu w tym kinie.")
-        continue
-    miejsce_uzytkownika = input("Podaj miejsce ")
-    if not int(miejsce_uzytkownika) in range(1,9):
-        print("Niestety nie mamy takiego miejsca.")
-        continue
-    if int(rzad_uzytkownika) == 1 and int(miejsce_uzytkownika) == 1:
-        if not rzad_1_miejsce_1:
-            rzad_1_miejsce_1 = True
-            #ilosc_miejsc_sprzedanych += 1
-            ilosc_miejsc_sprzedanych_1_rzedzie += 1
-        else:
-            print("Niestety, to miejsce jest już zajęte.")
+    print("Podaj co chcesz zrobić w naszym programie.\n"
+          "1. Zrób rezerwację miejsca\n"
+          "2. Zakończ program i wyświetl nam potrzebne informacje.")
+    wybor_menu = input("Wybierz opcję. ")
+    if int(wybor_menu) == 1:
+        rzad_uzytkownika = input("Podaj rząd, w którym chcesz zarezerwować miejsce ")
+        if not int(rzad_uzytkownika) in range (1,3):
+            print("Niestety nie mamy takiego rzędu w tym kinie.")
             continue
-    elif int(rzad_uzytkownika) == 1 and int(miejsce_uzytkownika) == 2:
-        if not rzad_1_miejsce_2:
-            rzad_1_miejsce_2 = True
-            ilosc_miejsc_sprzedanych_1_rzedzie += 1
+        miejsce_uzytkownika = input("Podaj miejsce ")
+        if not int(miejsce_uzytkownika) in range(1,9):
+            print("Niestety nie mamy takiego miejsca.")
+            continue
+        if int(rzad_uzytkownika) == 1 and int(miejsce_uzytkownika) == 1:
+            if not rzad_1_miejsce_1:
+                rzad_1_miejsce_1 = True
+                #ilosc_miejsc_sprzedanych += 1
+                ilosc_miejsc_sprzedanych_1_rzedzie += 1
+            else:
+                print("Niestety, to miejsce jest już zajęte.")
+                continue
+        elif int(rzad_uzytkownika) == 1 and int(miejsce_uzytkownika) == 2:
+            if not rzad_1_miejsce_2:
+                rzad_1_miejsce_2 = True
+                ilosc_miejsc_sprzedanych_1_rzedzie += 1
+            else:
+                print("Niestety, to miejsce jest już zajęte.")
+        elif int(rzad_uzytkownika) == 1 and int(miejsce_uzytkownika) == 3:
+            if not rzad_1_miejsce_3:
+                rzad_1_miejsce_3 = True
+                ilosc_miejsc_sprzedanych_1_rzedzie += 1
+            else:
+                print("Niestety, to miejsce jest już zajęte.")
+        elif int(rzad_uzytkownika) == 1 and int(miejsce_uzytkownika) == 4:
+            if not rzad_1_miejsce_4:
+                rzad_1_miejsce_4 = True
+                ilosc_miejsc_sprzedanych_1_rzedzie += 1
+            else:
+                print("Niestety, to miejsce jest już zajęte.")
+        elif int(rzad_uzytkownika) == 1 and int(miejsce_uzytkownika) == 5:
+            if not rzad_1_miejsce_5:
+                rzad_1_miejsce_5 = True
+                ilosc_miejsc_sprzedanych_1_rzedzie += 1
+            else:
+                print("Niestety, to miejsce jest już zajęte.")
+        elif int(rzad_uzytkownika) == 1 and int(miejsce_uzytkownika) == 6:
+            if not rzad_1_miejsce_6:
+                rzad_1_miejsce_6 = True
+                ilosc_miejsc_sprzedanych_1_rzedzie += 1
+            else:
+                print("Niestety, to miejsce jest już zajęte.")
+        elif int(rzad_uzytkownika) == 1 and int(miejsce_uzytkownika) == 7:
+            if not rzad_1_miejsce_7:
+                rzad_1_miejsce_7 = True
+                ilosc_miejsc_sprzedanych_1_rzedzie += 1
+            else:
+                print("Niestety, to miejsce jest już zajęte.")
+        elif int(rzad_uzytkownika) == 1 and int(miejsce_uzytkownika) == 8:
+            if not rzad_1_miejsce_8:
+                rzad_1_miejsce_8 = True
+                ilosc_miejsc_sprzedanych_1_rzedzie += 1
+            else:
+                print("Niestety, to miejsce jest już zajęte.")
+        elif int(rzad_uzytkownika) == 2 and int(miejsce_uzytkownika) == 1:
+            if not rzad_2_miejsce_1:
+                rzad_2_miejsce_1 = True
+                ilosc_miejsc_sprzedanych_2_rzedzie += 1
+            else:
+                print("Niestety, to miejsce jest już zajęte.")
+        elif int(rzad_uzytkownika) == 2 and int(miejsce_uzytkownika) == 2:
+            if not rzad_2_miejsce_2:
+                rzad_2_miejsce_2 = True
+                ilosc_miejsc_sprzedanych_2_rzedzie += 1
+            else:
+                print("Niestety, to miejsce jest już zajęte.")
+        elif int(rzad_uzytkownika) == 2 and int(miejsce_uzytkownika) == 3:
+            if not rzad_2_miejsce_3:
+                rzad_2_miejsce_3 = True
+                ilosc_miejsc_sprzedanych_2_rzedzie += 1
+            else:
+                print("Niestety, to miejsce jest już zajęte.")
+        elif int(rzad_uzytkownika) == 2 and int(miejsce_uzytkownika) == 4:
+            if not rzad_2_miejsce_4:
+                rzad_2_miejsce_4 = True
+                ilosc_miejsc_sprzedanych_2_rzedzie += 1
+            else:
+                print("Niestety, to miejsce jest już zajęte.")
+        elif int(rzad_uzytkownika) == 2 and int(miejsce_uzytkownika) == 5:
+            if not rzad_2_miejsce_5:
+                rzad_2_miejsce_5 = True
+                ilosc_miejsc_sprzedanych_2_rzedzie += 1
+            else:
+                print("Niestety, to miejsce jest już zajęte.")
+        elif int(rzad_uzytkownika) == 2 and int(miejsce_uzytkownika) == 6:
+            if not rzad_2_miejsce_6:
+                rzad_2_miejsce_6 = True
+                ilosc_miejsc_sprzedanych_2_rzedzie += 1
+            else:
+                print("Niestety, to miejsce jest już zajęte.")
+        elif int(rzad_uzytkownika) == 2 and int(miejsce_uzytkownika) == 7:
+            if not rzad_2_miejsce_7:
+                rzad_2_miejsce_7 = True
+                ilosc_miejsc_sprzedanych_2_rzedzie += 1
+            else:
+                print("Niestety, to miejsce jest już zajęte.")
         else:
-            print("Niestety, to miejsce jest już zajęte.")
-    elif int(rzad_uzytkownika) == 1 and int(miejsce_uzytkownika) == 3:
-        if not rzad_1_miejsce_3:
-            rzad_1_miejsce_3 = True
-            ilosc_miejsc_sprzedanych_1_rzedzie += 1
-        else:
-            print("Niestety, to miejsce jest już zajęte.")
+            if not rzad_2_miejsce_8:
+                rzad_2_miejsce_8 = True
+                ilosc_miejsc_sprzedanych_2_rzedzie += 1
+            else:
+                print("Niestety, to miejsce jest już zajęte.")
+    elif int(wybor_menu) == 2:
+        koniec_programu = True
+print("Zakończyliśmy działanie programu! Oto statystyki z Twojej sprzedaży: ")
+print(f"Ilość sprzedanych biletów to: {ilosc_miejsc_sprzedanych_1_rzedzie + ilosc_miejsc_sprzedanych_2_rzedzie}")
+print(f"Procentowo jest to: {((ilosc_miejsc_sprzedanych_1_rzedzie + ilosc_miejsc_sprzedanych_2_rzedzie)/ilosc_miejsc) * 100}")
+#for rzad in ilosc_rzedow
+if ilosc_miejsc_sprzedanych_1_rzedzie > ilosc_miejsc_sprzedanych_2_rzedzie:
+    print(f"Najwięcej miejsc sprzedanych w 1 rzędzie. Było to: {ilosc_miejsc_sprzedanych_1_rzedzie} biletów")
+elif ilosc_miejsc_sprzedanych_2_rzedzie > ilosc_miejsc_sprzedanych_1_rzedzie:
+    print(f"Najwięcej miejsc sprzedanych w 2 rzędzie. Było to: {ilosc_miejsc_sprzedanych_2_rzedzie} biletów")
+else:
+    print(f"Sprzedaliśmy po tyle samo biletów w obydwu rzędach. Było to: {ilosc_miejsc_sprzedanych_1_rzedzie} biletów")
